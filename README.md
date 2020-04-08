dcf0eec2e68f90db04dd9682d3e9ac5b51f91286
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-04-08 12:21:41 -0700

    everything broken, trying to do click

--- README.md
+++ README.md
@@ -2,13 +2,24 @@
 # author : haley Griffin
 
 # time estimates
-
+## Feature 1
 Number and name of feature: #1 display images
 
 Estimate of time needed to complete: 30 minutes?
 
-Start time: 9:15 am
+Start time: 9:17 am
 
-Finish time: _____
+Finish time: 10:14 am
 
-Actual time needed to complete: _____
+Actual time needed to complete: 1 hour
+
+## feature 2 filter images
+Number and name of feature: #1 display images
+
+Estimate of time needed to complete: 60 minutes?
+
+Start time: 10:16 am
+
+Finish time:  am
+
+Actual time needed to complete: 
--- js/app.js
+++ js/app.js
@@ -1,35 +1,61 @@
 'use strict';
 
-function ImageGallary(image_url, title, description, keyword, horns){
-  this.image_url = image_url; 
-  this.title = title;
-  this.description = description;
-  this.keyword = keyword; 
-  this.horns = horns;
+let gallery = [];
+
+function ImageGallery(image_url, title, description, keyword, horns){
+    this.image_url = image_url;
+    this.title = title;
+    this.description = description;
+    this.keyword = keyword;
+    this.horns = horns;
+    gallery.push(this);
 }
 
-ImageGallary.prototype.render = () => {
-  $('#body').append(`
-    <h2>${this.title}</h2>
-    <img src=${this.image_url} alt=${this.keyword}>
-    <p>${this.description}</p>
-    <p>${this.keyword}</p>
-    <p>${this.horns}</p>
-    `);
-};
+// ImageGallery.prototype.render = () => {
+//   $('#body').append(`
+//     <h2>${this.title}</h2>
+//     <img src=${this.image_url} alt=${this.keyword}>
+//     <p>${this.description}</p>
+//     <p>${this.keyword}</p>
+//     <p>${this.horns}</p>
+//     `);
+// };
 
-console.log($.ajax('../data/page-1.json'));
-
-const AddValuesToBody = (item) => {
-  $('#body').append(`
-    <h2>${item.title}</h2>
-    <img src=${item.image_url}>
-    <p>${item.description}</p>
-    <p>${item.keyword}</p>
-    <p>${item.horns}</p>
-    `);
-};
-
-$.ajax('../data/page-1.json').then(data => {
-    data.forEach(AddValuesToBody);
+$('select').on('click', function(event) {
+    // $('#photoTemplate').hide();
+    console.log(event);
 });
+
+console.log($.ajax('data/page-1.json'));
+
+const addValuesToBody = (item) => {
+    $('#main').append(`
+        <article id='#photoTemplate>
+            <h2>${item.title}</h2>
+            <img src=${item.image_url}>
+            <p>${item.description}</p>
+            <p>${item.keyword}</p>
+            <p>${item.horns}</p>
+        </article>
+        `);
+};
+
+const addValuesToDropdown = (option) => {
+    $('select').append(`
+    <option value ='${option.keyword}'>${option.keyword}</option>
+    `);
+};
+
+$.ajax('data/page-1.json').then(data => {
+    data.forEach( (value) => {
+        new ImageGallery(value.image_url. value.title, value.description, value.keyword, value.horns);
+    });
+    gallery.forEach( value => {
+        addValuesToBody(value);
+        addValuesToDropdown(value);
+    });
+});
+
+console.log(gallery);
+
+
